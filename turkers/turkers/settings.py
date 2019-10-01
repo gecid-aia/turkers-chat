b8b4d910489b4d93cd0278dc5d8cd9e13b1f65a5
Joana Souza <souzajoana18@gmail.com>
2019-10-01 20:23:48 -0300

    Adicionando Auth_user_model  para model de usuario

--- turkers/turkers/settings.py
+++ turkers/turkers/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'usuarios.Usuario'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
