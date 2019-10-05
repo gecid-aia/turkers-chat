81b873e3f1351b88d44558d68526dfc4e7a128ec
Joana Souza <souzajoana18@gmail.com>
2019-10-04 22:19:22 -0300

    adicionando url de sobre

--- turkers/turkers/urls.py
+++ turkers/turkers/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from users.views import AboutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', AboutView.as_view(), name='sobre'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
