7fe33cc8f0aad0d3df7f9f712f45e26c3014beed
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-12 17:25:28 -0300

    Config 2-steps registration

--- turkers/turkers/urls.py
+++ turkers/turkers/urls.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from django_registration.backends.activation.views import RegistrationView
+
+from users.forms import UserRegistrationForm
 from users.views import AboutView
 
 urlpatterns = [
+    path('register/',
+        RegistrationView.as_view(form_class=UserRegistrationForm),
+        name='django_registration_register',
+    ),
+    path(r'', include('django_registration.backends.activation.urls')),
+    path(r'', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('sobre/', AboutView.as_view(), name='sobre'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
