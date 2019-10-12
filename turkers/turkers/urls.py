491698b7ad3e5490e78f72304fd570cfd234ef48
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-12 18:10:12 -0300

    Login user after account activation

--- turkers/turkers/urls.py
+++ turkers/turkers/urls.py
@@ -6,12 +6,16 @@ from django.conf.urls.static import static
 from django_registration.backends.activation.views import RegistrationView
 
 from users.forms import UserRegistrationForm
-from users.views import AboutView
+from users.views import AboutView, ActivationView
 
 urlpatterns = [
     path('register/',
         RegistrationView.as_view(form_class=UserRegistrationForm),
         name='django_registration_register',
+    ),
+    path('activate/<activation_key>/',
+        ActivationView.as_view(),
+        name='django_registration_activate'
     ),
     path(r'', include('django_registration.backends.activation.urls')),
     path(r'', include('django.contrib.auth.urls')),
--- turkers/users/views.py
+++ turkers/users/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth import login
+
+from django_registration.backends.activation.views import ActivationView as BaseActivationView
+
 
 class AboutView(TemplateView):
     template_name = "about.html"
+
+
+class ActivationView(BaseActivationView):
+
+    def activate(self, *args, **kwargs):
+        user = super().activate(*args, **kwargs)
+        login(self.request, user)
+        return user
