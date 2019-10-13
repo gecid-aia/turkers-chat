ea736e9ada46e24b34921e7f2ca7db49141903a8
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-13 10:40:05 -0300

    Structure endpoints

--- turkers/chats/api_urls.py
+++ turkers/chats/api_urls.py
@@ -2,11 +2,13 @@ from rest_framework_swagger.views import get_swagger_view
 
 from django.urls import path
 
-from chats import views
+from chats import api_views as views
 
 schema_view = get_swagger_view(title='Exchange With Turkers API')
 
 app_name = 'chats_api'
 urlpatterns = [
-    path('', schema_view)
+    path('', schema_view),
+    path('chat/colective/', views.ColectiveChatEndpoint.as_view(), name='colective'),
+    path('chat/turker/<int:turker_id>/', views.ColectiveChatEndpoint.as_view(), name='turker'),
 ]
---
+++ turkers/chats/api_views.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+
+
+class ColectiveChatEndpoint(APIView):
+    pass
+
+
+class TurkerChatEndpoint(APIView):
+    pass
