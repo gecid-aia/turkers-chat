19f6b03baaf68d8a84131a37d873ac6d2f2c2476
Luciano Ratamero <luciano@ratamero.com>
2019-11-25 00:21:26 -0300

    Adiciona app react base para o chat

---
+++ turkers/chats/react/src/js/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { eventsMiddleware } from "rel-events";
+
+import rootReducer from "./store/reducers";
+
+import "whatwg-fetch";
+import "../scss/index.scss";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, eventsMiddleware)
+);
+
+class App extends React.Component {
+  render() {
+    return <Provider store={store}></Provider>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("chat-react"));
