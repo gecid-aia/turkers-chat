31ea2bdd5d5cebde7a4b5caee500dcb0af4fba72
Luciano Ratamero <luciano@ratamero.com>
2019-12-07 17:17:21 -0300

    Adiciona página de chats

---
+++ turkers/chats/react/src/js/components/Chat/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ChatBox from './ChatBox';
+import { GetChatsEvent } from '../../events';
+
+class ChatsPage extends React.Component {
+  componentDidMount(){ this.props.getChats(); }
+
+  render(){
+    return this.props.chats.map((chat) => (
+      <ChatBox messagesUrl={chat.messages_url} chatTitle={chat.title} chatIsCollective={chat.is_collective} />
+    ));
+  }
+}
+
+export default GetChatsEvent.register({
+  Component: ChatsPage,
+  props: ['chats']
+});
