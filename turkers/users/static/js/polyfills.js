502e391b0668f68a62ffdb0409dfb1aa035b4d39
Bernardo Fontes <bernardoxhc@gmail.com>
2020-03-09 21:40:54 -0300

    Configurações de polyfill para arrays e strings

--- turkers/users/static/js/polyfills.js
+++ turkers/users/static/js/polyfills.js
@@ -2,3 +2,30 @@ const objectToValuesPolyfill = function(object){
   return Object.keys(object).map(function(key){ return object[key]});
 };
 Object.values = Object.values || objectToValuesPolyfill;
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, "includes", {
+    enumerable: false,
+    value: function(obj) {
+        var newArr = this.filter(function(el) {
+          return el == obj;
+        });
+        return newArr.length > 0;
+      }
+  });
+}
