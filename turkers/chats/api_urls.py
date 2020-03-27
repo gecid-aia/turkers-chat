b8a9f7c19d7c715c791ac206f396b6d49770d870
Bernardo Fontes <bernardoxhc@gmail.com>
2020-03-27 15:22:48 -0300

    Desativa o swagger

--- turkers/chats/api_urls.py
+++ turkers/chats/api_urls.py
@@ -1,14 +1,9 @@
-from rest_framework_swagger.views import get_swagger_view
-
 from django.urls import path
 
 from chats import api_views as views
 
-schema_view = get_swagger_view(title="Exchange With Turkers API")
-
 app_name = "chats_api"
 urlpatterns = [
-    path("", schema_view),
     path("chats/", views.UserAvailableChatsEndpoint.as_view(), name="chats_index"),
     path("chat/<int:chat_id>/", views.ChatEndpoint.as_view(), name="chat"),
     path(
